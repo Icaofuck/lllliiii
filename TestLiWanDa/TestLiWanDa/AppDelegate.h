2974145312e3c04f33c17ecc01e7afdb0cf2fa71
lanou3g <1150922429@qq.com>
2016-03-17 09:39:25 +0800

    第一次提交:

---
+++ TestLiWanDa/TestLiWanDa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLiWanDa
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 李万达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLiWanDa/TestLiWanDa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLiWanDa
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 李万达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
